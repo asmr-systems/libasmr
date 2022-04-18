5d19d4e96f19a7556f90af5408d5ef36dd62c7da
c <0.0@asmr.software>
2022-04-18 18:43:56 -0400

    implement pinMode

--- arch/samd/core/digital_io.cc
+++ arch/samd/core/digital_io.cc
@@ -9,14 +9,26 @@ void pinMode(pin_size_t pinNumber, PinMode pinMode) {
     switch (pinMode) {
     case PinMode::INPUT:
         PORT->Group[g_PinTable[pin].port].PINCFG[g_PinTable[pin].pin].reg = (uint8_t)(PORT_PINCFG_INEN);
+        PORT->Group[g_PinTable[pin].port].DIRCLR[g_PinTable[pin].pin].reg = (uint32_t)(1<<g_PinTable[pin].pin);
         break;
     case PinMode::OUTPUT:
+        // set to output with input enabled (for reading current value of outputs)
+        PORT->Group[g_PinTable[pin].port].PINCFG[g_PinTable[pin].pin].reg = (uint8_t)(PORT_PINCFG_INEN);
+        PORT->Group[g_PinTable[pin].port].DIRSET[g_PinTable[pin].pin].reg = (uint32_t)(1<<g_PinTable[pin].pin);
         break;
     case PinMode::INPUT_PULLUP:
+        PORT->Group[g_PinTable[pin].port].PINCFG[g_PinTable[pin].pin].reg = (uint8_t)(PORT_PINCFG_INEN|PORT_PINCFG_PULLEN);
+        PORT->Group[g_PinTable[pin].port].DIRCLR[g_PinTable[pin].pin].reg = (uint32_t)(1<<g_PinTable[pin].pin);
+
+        // Enable pull level (cf '22.6.3.2 Input Configuration' and '22.8.7 Data Output Value Set')
+        PORT->Group[g_PinTable[pin].pin].OUTSET.reg = (uint32_t)(1<<g_PinTable[pin].pin) ;
         break;
     case PinMode::INPUT_PULLDOWN:
-        break;
-    case PinMode::OUTPUT_OPENDRAIN:
+        PORT->Group[g_PinTable[pin].port].PINCFG[g_PinTable[pin].pin].reg = (uint8_t)(PORT_PINCFG_INEN|PORT_PINCFG_PULLEN);
+        PORT->Group[g_PinTable[pin].port].DIRCLR[g_PinTable[pin].pin].reg = (uint32_t)(1<<g_PinTable[pin].pin);
+
+        // Enable pull level (cf '22.6.3.2 Input Configuration' and '22.8.7 Data Output Value Clear')
+        PORT->Group[g_PinTable[pin].pin].OUTCLR.reg = (uint32_t)(1<<g_PinTable[pin].pin) ;
         break;
     default:
         break;
